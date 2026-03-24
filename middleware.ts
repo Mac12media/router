b7bec73ac63ccc9161dbd5ab843ab9a8cbb8c8a4
Mac12media <mac12media@gmail.com>
2026-03-23 19:42:10 -0500

    try

--- middleware.ts
+++ middleware.ts
@@ -2,6 +2,6 @@ export { auth as middleware } from "@/lib/auth";
 
 export const config = {
   matcher: [
-    "/((?!api|_next/static|_next/image|favicon.ico|profile|check-email|sport-icons|.*\\.(?:svg|png|jpg|jpeg|gif|webp|avif|ico)$).*)",
+    "/((?!api|_next/static|_next/image|favicon.ico|profile|check-email|sport-icons|posts/[^/]+(?:/opengraph-image)?|.*\\.(?:svg|png|jpg|jpeg|gif|webp|avif|ico)$).*)",
   ],
 };
