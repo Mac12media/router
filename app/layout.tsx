ae92115e0a5e63e569539549291930cd345635a5
Mac12media <mac12media@gmail.com>
2026-04-07 18:21:05 -0500

    try

--- app/layout.tsx
+++ app/layout.tsx
@@ -13,7 +13,7 @@ const font = Inter({ subsets: ["latin"] });
 
 export const metadata: Metadata = {
   metadataBase: new URL(
-    process.env.NEXT_PUBLIC_APP_URL || "http://localhost:3000"
+    process.env.NEXT_PUBLIC_APP_URL || "https://app.exporecruits.com"
   ),
   title: "EXPO",
   description:
